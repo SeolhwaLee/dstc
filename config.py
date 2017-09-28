a35dad5b7876f96d472131d0788d766945bcab10
sseol11 <sseol11@github.com>
2017-09-28 22:16:55 +0900

    Add new files

---
+++ config.py
@@ -0,0 +1,34 @@
+import os
+from general_utils import get_logger
+
+
+class Config():
+    def __init__(self):
+        # directory for training outputs
+        if not os.path.exists(self.output_path):
+            os.makedirs(self.output_path)
+        self.logger = get_logger(self.log_path)
+
+    output_path = 'results/mlp/'
+    model_output = output_path + 'model.weights/'
+    log_path = output_path + "log.txt"
+
+    reload = False
+
+    lr = 0.001
+    lr_decay = 0.9
+    beta = 0.01
+    num_layer = 5
+    num_epochs = 20
+    batch_size = 10
+    nepoch_no_imprv = 3
+
+
+    embed_method = 'word2vec'
+
+    # file name lists for training
+    word2vec_filename = '../bilingual/wiki/wiki_en_model'
+
+    train_filename = './data/train_dataset'
+    dev_filename = './data/dev_dataset'
+    test_filename = './data/test_dataset'
---
+++ train.py
@@ -0,0 +1,28 @@
+from config import Config
+from dnn_model import Dnn
+from data_process import Data
+from utterance_embed import UtteranceEmbed
+
+
+def main(config):
+    data = Data(config.train_filename, config.dev_filename, config.test_filename)
+    train_data = data.train_set
+    dev_data = data.dev_set
+    test_data = data.test_set
+
+    # load word2vec
+    utter_embed = UtteranceEmbed(config.word2vec_filename)
+
+    input_size = (utter_embed.get_vector_size() * 2) # concat size
+
+    num_neurons = [750, 750, 500, 250]
+
+    model = Dnn(input_size, num_neurons, 3, utter_embed, config)
+    model.build()
+    model.train(train_data, dev_data, test_data)
+
+
+if __name__ == "__main__":
+    config = Config()
+
+    main(config)
